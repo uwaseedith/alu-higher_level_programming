81390055ec5febb967844b9d8cc19a89b21144ae
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-10-26 13:40:46 +0200

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,7 @@
-6-print_comb3.py
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j))
+            break
+        print("{}{}".format(i, j), end=", ")
