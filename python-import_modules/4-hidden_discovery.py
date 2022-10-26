4ef373e975564ff9d692f05b6cb45624069dcbc8
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-10-27 01:14:02 +0200

    Update 4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,8 @@
-4-hidden_discovery.py
+#!/usr/bin/python3
+import hidden_4
+if __name__ == '__main__':
+    names = dir(hidden_4)
+    names.sort()
+    for attr in names:
+        if attr[:2] != "__":
+            print(attr)
