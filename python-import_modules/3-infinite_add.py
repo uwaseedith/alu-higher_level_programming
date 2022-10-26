68d70044665cbe941e3ba6e989c37c330b0a7e89
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-10-27 01:13:11 +0200

    Update 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,8 @@
-3-infinite_add.py
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
