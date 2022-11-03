3b373d5dc300fdb46b369814fc8684fac85215aa
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-03 11:16:06 +0200

    Update 8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,7 @@
-8-multiple_returns.py
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence:
+        sentence_length = len(sentence)
+    else:
+        sentence_length = 0
+    return tuple((sentence_length, None if not sentence else sentence[:1]))
