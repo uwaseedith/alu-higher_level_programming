d06d705f0774793452f59dc6f41045836f70d5d5
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-03 11:13:21 +0200

    Update 4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,1 +1,9 @@
-4-new_in_list.py
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    n_list = list(my_list)
+    n_list[idx] = element
+    return n_list
