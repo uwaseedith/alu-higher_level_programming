f89e884e57a961c8f82d9a696944198a0f0ded63
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-03 11:14:21 +0200

    Update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,10 @@
-6-print_matrix_integer.py
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for list_ in matrix:
+        if not list_:
+            print()
+        for item in list_:
+            if item == list_[-1]:
+                print('{:d}'.format(item))
+            else:
+                print('{:d}'.format(item), end=" ")
