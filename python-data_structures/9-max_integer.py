13936305b80fe2a5f4ff892b4586d517aa9f4621
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-03 11:16:38 +0200

    Update 9-max_integer.py

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,1 +1,9 @@
-9-max_integer.py
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    max = my_list[0]
+    for item in my_list:
+        if item > max:
+            max = item
+    return max
