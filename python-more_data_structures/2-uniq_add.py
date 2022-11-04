21c67b4060c165056fc18178f6f23a0bd40cb633
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-04 22:47:38 +0200

    Update 2-uniq_add.py

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,7 @@
-2-uniq_add.py
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_list = list()
+    for item in my_list:
+        if item not in uniq_list:
+            uniq_list.append(item)
+    return sum(uniq_list)
