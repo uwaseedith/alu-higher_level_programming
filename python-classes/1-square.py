672dc41375e573c659f2587db793b0afcfceb268
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-12 11:46:50 +0200

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,9 @@
-1-square.py
+#!/usr/bin/python3
+"""Write a class Square that defines a square by"""
+
+
+class Square:
+    """Private instance attribute: size
+    Instantiation with size (no type/value verification)"""
+    def __init__(self, size):
+        self.__size = size
