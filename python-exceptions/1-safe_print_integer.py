28790683a85ef98a4f1a5638c3321e91c577ee47
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-12 11:30:00 +0200

    Update 1-safe_print_integer.py

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,8 @@
--safe_print_integer.py
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
