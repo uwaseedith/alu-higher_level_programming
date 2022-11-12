b1fd7e8f5d2478180a482eb561b6c4ad442ac565
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-12 12:11:25 +0200

    Update 0-rectangle.py

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,1 +1,7 @@
-0-rectangle.py
+#!/usr/bin/python3
+"""Write an empty class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """Write an empty class Rectangle that defines a rectangle"""
+    pass
