8df6d31d1912910e1e77f7fbdb6cfbe8cd7511fe
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-18 16:06:22 +0200

    Update 5-post_params.sh

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,1 +1,3 @@
-5-post_params.sh
+#!/bin/bash
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
