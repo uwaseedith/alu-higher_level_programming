089e4c52e11d857523467eabd059c20b6dccd458
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-18 15:29:50 +0200

    Update 6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,1 +1,13 @@
-5-save_to_json_file.py
+#!/usr/bin/python3
+"""
+Module 6-load_from_json_file.py
+"""
+
+
+def load_from_json_file(filename):
+    """“JSON file”"""
+
+    import json
+
+    with open(filename, mode="r", encoding="utf-8") as f:
+        return json.load(f)
