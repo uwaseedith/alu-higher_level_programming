1b6080ad347e6871d95829d53f0cf8653d8e291a
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-18 15:28:15 +0200

    Update 4-from_json_string.py

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,11 @@
-4-from_json_string.py
+#!/usr/bin/python3
+"""
+Module 4-from_json_string.py
+"""
+
+
+def from_json_string(my_str):
+    """returns python data structure frm JSON"""
+    import json
+
+    return json.loads(my_str)
