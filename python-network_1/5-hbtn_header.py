94d188601367959882ae95c3eb2d841be87312b9
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-21 00:43:50 +0200

    Update 5-hbtn_header.py

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,1 +1,14 @@
-5-hbtn_header.py
+#!/usr/bin/python3
+"""Check status"""
+import requests
+import sys
+
+
+def header():
+    """status"""
+    result = requests.get(sys.argv[1])
+
+    print(result.headers.get("X-Request-Id", None))
+
+if __name__ == "__main__":
+    header()
