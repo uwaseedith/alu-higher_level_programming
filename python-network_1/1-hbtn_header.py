aab5808889b5a4076dce23591dc138d1cda8fcfa
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-21 00:41:04 +0200

    Update 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,1 +1,14 @@
-1-hbtn_header.py
+#!/usr/bin/python3
+"""Fetches header"""
+import urllib.request
+import sys
+
+
+def fetcher():
+    """fetcher"""
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        header = response.info()
+        print(header["X-Request-Id"])
+
+if __name__ == "__main__":
+    fetcher()
