84db3fa453b7a83df5d9c72b6284657404c1fcb0
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-21 12:14:25 +0200

    Update 16-shows_by_genre.sql

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,1 +1,8 @@
-16-shows_by_genre.sql
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT a.title, c.name
+FROM tv_shows a
+LEFT JOIN tv_show_genres b
+ON a.id = b.show_id
+LEFT JOIN tv_genres c
+ON b.genre_id = c.id
+ORDER BY a.title ASC, c.name ASC;
