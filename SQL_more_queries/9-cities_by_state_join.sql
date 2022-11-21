d0b0dbff07500730cd3d2e9d2ad36a7ff4763a26
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-21 12:23:48 +0200

    Update 9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,6 @@
-9-cities_by_state_join.sql
+-- lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
