f945758e09c6a2cb9e2b5a4c1f2c46a909293a56
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-21 12:03:04 +0200

    Update 10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,1 +1,5 @@
-10-genre_id_by_show.sql
+-- lists all shows contained in hbtn_0d_tvshows
+SELECT a.title, b.genre_id
+FROM tv_shows a, tv_show_genres b
+WHERE b.show_id = a.id
+ORDER BY a.title ASC, b.genre_id ASC;
