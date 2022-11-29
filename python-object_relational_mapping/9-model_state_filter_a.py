025ea3ebd5fc6d6df255c0c5666aa22e7bf199dd
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-11-29 21:10:25 +0200

    Update 9-model_state_filter_a.py

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,20 @@
-9-model_state_filter_a.py
+#!/usr/bin/python3
+""" list objects"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import State, Base
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    st = session().query(State).filter(State.name.like('%a%')).order_by(
+        State.id).all()
+    if st:
+        for stat in st:
+            if 'a' in stat.name:
+                print("{}: {}".format(stat.id, stat.name))
+    session().close()
