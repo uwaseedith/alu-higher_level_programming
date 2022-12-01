86fcadfe4f4ba2ab07b989b3082a86405c9f9dbd
uwaseedith <109415728+uwaseedith@users.noreply.github.com>
2022-12-01 23:15:19 +0200

    Update 8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""List first State objects from db"""
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
+""" just first fetch """
 
-
-def list_first_state_obj():
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-
-    f = session.query(State).first()
-
-    if f:
-        print("{}: {}".format(f.__dict__['id'], f.__dict__['name']))
-    else:
-        print("Nothing")
-
-    session.close()
 
 if __name__ == "__main__":
-    list_first_state_obj()
+    from sys import argv
+    from model_state import State, Base
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+    st = session().query(State).first()
+    if st:
+        print("{}: {}".format(st.id, st.name))
+    else:
+        print("Nothing")
+    session().close()
