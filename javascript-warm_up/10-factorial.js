b8b395fe77582328f326e9649f57f3bfd46a558e
Uwaseedith <e.uwase@alustudent.com>
2023-02-07 23:19:08 +0200

    ololo

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2], 10);
+
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
+console.log(factorial(num));
